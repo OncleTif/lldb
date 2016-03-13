f35a2e1017108b9f74a6cdae0fa4d6eb6039bdf2
tmanet <tmanet@student.42.fr>
2016-03-13 18:30:45 +0100

    couleur

--- 03-bonus/import_conf.py
+++ 03-bonus/import_conf.py
@@ -2,14 +2,15 @@ import lldb
 import os.path
 
 def import_conf(debugger, command, result, internal_dict):
-	s = str(debugger.GetSelectedTarget()) + ".config"
-	if os.path.isfile(s):
-		debugger.HandleCommand("command source " + s);
-		return
-	if os.path.isfile("commandes"):
-		debugger.HandleCommand("command source commandes");
-		return
+    s = str(debugger.GetSelectedTarget()) + ".config"
+    if os.path.isfile(s):
+        debugger.HandleCommand('script print "\033[32m[Loading ' + s + ']\033[0m"')
+        debugger.HandleCommand("command source " + s);
+        return
+    if os.path.isfile("commandes"):
+        debugger.HandleCommand('script print "\033[32m[Loading \"commandes\"]\033[0m"')
+        debugger.HandleCommand("command source commandes");
+        return
 
 def __lldb_init_module(debugger, dict):
-	debugger.HandleCommand('command script add -f import_conf.import_conf import_config');
-	debugger.HandleCommand('import_config');
+    debugger.HandleCommand('command script add -f import_conf.import_conf import -h "This command detect a <file>.config file or commandes file and execute its commands');
