b06d2fbf6b1deae8f919bebd911cf54bea535990
Alexandre DESPONDS <adespond@e1r2p2.42.fr>
2016-03-13 20:29:26 +0100

    a

--- 02-script/reverse.py
+++ 02-script/reverse.py
@@ -1,8 +1,9 @@
 import lldb
 
 def reverse(debugger, command, result, internal_dict):
-	s = str(debugger.GetSelectedTarget())
-	print("FT_" + s[::-1])
+	if debugger.GetSelectedTarget():
+		s = str(debugger.GetSelectedTarget())
+		print("FT_" + s[::-1])
 
 def __lldb_init_module(debugger, dict):
 	debugger.HandleCommand('command script add -f reverse.reverse reverse')  
