731b5ad9acfb345ffbf82c4a8ba18b042ea880ef
tmanet <tmanet@student.42.fr>
2016-03-13 18:10:48 +0100

    ajout message d'help

--- 03-bonus/init.py
+++ 03-bonus/init.py
@@ -3,6 +3,8 @@ import lldb
 def init(debugger, command, result, internal_dict):
     debugger.HandleCommand('breakpoint set -name main')
     debugger.HandleCommand('run')
+    debugger.HandleCommand('script print "\033[32mThread STARTED and Waiting\033[0m"')
+
 
 def __lldb_init_module(debugger, dict):
-	debugger.HandleCommand('command script add -f init.init init')
+	debugger.HandleCommand('command script add -f init.init init -h "init puts a breakpoint on main and run the process"')
